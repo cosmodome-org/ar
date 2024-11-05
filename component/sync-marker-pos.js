6b688929eb0b1b67e272e618a635fa85acb95fde
Bastien Sarolea <sarobastien@gmail.com>
2024-11-05 22:26:42 +0100

    03

---
+++ component/sync-marker-pos.js
@@ -0,0 +1,25 @@
+AFRAME.registerComponent("sync-marker-pos", {
+    init: function () {
+        this.localpos = new THREE.Vector3();
+        this.worldpos = new THREE.Vector3();
+        this.textEl = document.querySelector("p")
+    },
+    posToString: function (pos) {
+        return pos.x.toFixed(2) + " " + pos.y.toFixed(2) + " " + pos.z.toFixed(2);
+    },
+    // called on each frame
+    tick: function () {
+        // getAttribute("position") has the local position
+        // this.el.object3D.position has the local position
+        // this.el.getObject3D("mesh").position has the local position
+        this.localpos.copy(this.el.getAttribute("position"))
+        this.el.getObject3D("mesh").getWorldPosition(this.worldpos)
+        document.getElementById('all').setAttribute('position', posToString(this.worldpos))
+        // compose the displayed message
+        let msg = "";
+        msg += "Sphere local position:" + this.posToString(this.localpos)
+        msg += "<br>"
+        msg += "Sphere world position:" + this.posToString(this.worldpos)
+        this.textEl.innerHTML = msg
+    }
+})
