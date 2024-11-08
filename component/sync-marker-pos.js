e8f84c733414c047a6d8c88ee637e3d22efe9b40
Bastien Sarolea <sarobastien@gmail.com>
2024-11-08 15:40:56 +0100

    -2

--- component/sync-marker-pos.js
+++ component/sync-marker-pos.js
@@ -1,28 +1,28 @@
-AFRAME.registerComponent("sync-marker-pos", {
-    init: function () {
-        console.log('sync-marker-pos')
-        this.localpos = new THREE.Vector3();
-        this.worldpos = new THREE.Vector3();
-        this.textEl = document.querySelector("p")
-    },
-    posToString: function (pos) {
-        // return pos.x.toFixed(2) + " " + pos.y.toFixed(2) + " " + pos.z.toFixed(2);
-        return pos.x.toFixed(2) + " " + pos.y.toFixed(2) + " -9";
-    },
-    // called on each frame
-    tick: function () {
-        // getAttribute("position") has the local position
-        // this.el.object3D.position has the local position
-        // this.el.getObject3D("mesh").position has the local position
-        this.localpos.copy(this.el.getAttribute("position"))
-        this.el.getObject3D("mesh").getWorldPosition(this.worldpos)
-        document.getElementById('all').setAttribute('position', this.posToString(this.worldpos))
-        // compose the displayed message
-        // let msg = "";
-        // msg += "Sphere local position:" + this.posToString(this.localpos)
-        // msg += "<br>"
-        // msg += "Sphere world position:" + this.posToString(this.worldpos)
-        // console.log(msg)
-        // this.textEl.innerHTML = msg
-    }
-})
+// AFRAME.registerComponent("sync-marker-pos", {
+//     init: function () {
+//         console.log('sync-marker-pos')
+//         this.localpos = new THREE.Vector3();
+//         this.worldpos = new THREE.Vector3();
+//         this.textEl = document.querySelector("p")
+//     },
+//     posToString: function (pos) {
+//         // return pos.x.toFixed(2) + " " + pos.y.toFixed(2) + " " + pos.z.toFixed(2);
+//         return pos.x.toFixed(2) + " " + pos.y.toFixed(2) + " -9";
+//     },
+//     // called on each frame
+//     tick: function () {
+//         // getAttribute("position") has the local position
+//         // this.el.object3D.position has the local position
+//         // this.el.getObject3D("mesh").position has the local position
+//         this.localpos.copy(this.el.getAttribute("position"))
+//         this.el.getObject3D("mesh").getWorldPosition(this.worldpos)
+//         document.getElementById('all').setAttribute('position', this.posToString(this.worldpos))
+//         // compose the displayed message
+//         // let msg = "";
+//         // msg += "Sphere local position:" + this.posToString(this.localpos)
+//         // msg += "<br>"
+//         // msg += "Sphere world position:" + this.posToString(this.worldpos)
+//         // console.log(msg)
+//         // this.textEl.innerHTML = msg
+//     }
+// })
